cadc6e6e2616cc484c720351c7bd7ad033988937
Amanda <113999177+souxmand@users.noreply.github.com>
2023-11-04 00:58:49 -0300

    Update calculadora.js
    implementação de resposta da divisão por 0 e ajustes gerais no código

--- calculadora/calculadora.js
+++ calculadora/calculadora.js
@@ -1,44 +1,47 @@
 // resultado
 let result = document.getElementById("result");
 
-// input botões de numero
+// Botões numéricos e operadores
 const numericButtons = document.querySelectorAll(".numeric-button");
+const operatorButtons = document.querySelectorAll(".operator-button");
+
 for (const button of numericButtons) {
     button.addEventListener("click", () => input(button.textContent));
 }
 
-// clicar no número
-const operatorButtons = document.querySelectorAll(".operator-button");
 for (const button of operatorButtons) {
     button.addEventListener("click", () => inputOperator(button.textContent));
 }
 
-// Input numeros
-function input(num) {
-    let number = result.value;
-    result.value = number + num;
+// Input números e operadores
+function input(inputValue) {
+    result.value += inputValue;
 }
 
-// Input operadores
 function inputOperator(operator) {
     let expression = result.value;
 
-    // "Verifica se com um operador; se sim, substitui pelo novo operador."
     if (isOperator(expression.charAt(expression.length - 1))) {
         result.value = expression.substring(0, expression.length - 1) + operator;
     } else {
-        result.value = expression + operator;
+        result.value += operator;
     }
 }
 
-// Checar operador (+, -, *, /)
 function isOperator(char) {
     return char === '+' || char === '-' || char === '*' || char === '/';
 }
 
-// lógica
+// Lógica de cálculo
 function calc() {
     let expression = result.value;
+    if (expression === "") {
+        return;
+    }
+    if (expression.includes('/0')) {
+        alert("🤯Divisão por zero resulta no próprio número");
+        return;
+    }
     try {
         result.value = eval(expression);
     } catch (error) {
@@ -46,13 +49,11 @@ function calc() {
     }
 }
 
-// resetar
+// Limpar e Excluir
 function reset() {
     result.value = "";
 }
 
-// excluir 
 function del() {
-    let expression = result.value;
-    result.value = expression.substring(0, expression.length - 1);
+    result.value = result.value.slice(0, -1);
 }
